f0cb2386641b36e0c62ca33e6c7a027beed94ab2
Sanjin <102841251+duckduckgrayduck@users.noreply.github.com>
2025-03-27 12:00:51 -0500

    Try upping the sleep.

--- main.py
+++ main.py
@@ -105,7 +105,7 @@ class DocumentIntelligence(AddOn):
             for i in range(0, len(pages), page_chunk_size):
                 while True:
                     document_ref = self.client.documents.get(document.id)
-                    time.sleep(10)
+                    time.sleep(30)
                     if (
                         document_ref.status == "success"
                     ):  # Break out of for loop if document status becomes success
