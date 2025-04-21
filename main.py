11b4f02169c33570af227872490ca056fdf755f4
duckduckgrayduck <102841251+duckduckgrayduck@users.noreply.github.com>
2025-04-21 11:07:21 -0500

    Try 10 pages

--- main.py
+++ main.py
@@ -101,7 +101,7 @@ class DocumentIntelligence(AddOn):
 
                 pages.append(dc_page)
 
-            page_chunk_size = 20
+            page_chunk_size = 10
             max_retries = 5
             retry_delay = 60
 
