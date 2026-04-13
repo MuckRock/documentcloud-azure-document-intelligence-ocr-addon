0a80e6bf6d922fb080b09563279da7de54b6278e
Sanjin <102841251+duckduckgrayduck@users.noreply.github.com>
2026-04-13 20:25:53 +0000

    Update main.py

--- main.py
+++ main.py
@@ -57,6 +57,7 @@ class DocumentIntelligence(AddOn):
 
     def main(self):
         """The main add-on functionality goes here."""
+        self.client.session.headers.update({'User-Agent': 'Azure OCR Add-On'})
         if not self.validate():
             self.set_message("You do not have sufficient AI credits to run this Add-On")
             sys.exit(0)
